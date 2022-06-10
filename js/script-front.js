82506b079cdf3b1f1135541219c2a27033e77f3c
JohanAntonyAS <johan.antony.dev@gmail.com>
2022-06-09 20:40:24 -0500

    se agrego mensajes, responsive, y ocultar info

---
+++ js/script-front.js
@@ -0,0 +1,8 @@
+function hideInfo() {
+    let wsp = document.getElementById("Wsp");
+    wsp.classList.add("hideShow");
+}
+function showInfo() {
+    let wsp = document.getElementById("Wsp");
+    wsp.classList.remove("hideShow");
+}
