d83524752ae8badc7ce20f8a7548753acb387de9
JohanAntonyAS <johan.antony.dev@gmail.com>
2022-06-10 15:54:20 -0500

    mejora del textarea y espaciados de iconos, box contactos

--- js/script-front.js
+++ js/script-front.js
@@ -6,3 +6,15 @@ function showInfo() {
     let wsp = document.getElementById("Wsp");
     wsp.classList.remove("hideInfo");
 }
+
+// texrArea AutoResize
+let tx = document.getElementsByTagName('textarea');
+for (let i = 0; i < tx.length; i++) {
+    tx[i].setAttribute('style', 'height:' + (tx[i].scrollHeight) + 'px;overflow-y:hidden;');
+    tx[i].addEventListener("input", OnInput, false);
+}
+
+function OnInput(e) {
+    this.style.height = 'auto';
+    this.style.height = (this.scrollHeight) + 'px';
+}
